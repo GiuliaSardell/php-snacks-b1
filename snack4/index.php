0066965ab6faa68b9c380945897246eaa2e4b92f
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-12-21 11:08:05 +0100

    correzione in classe

--- snack4/index.php
+++ snack4/index.php
@@ -6,9 +6,10 @@ Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà co
 
 
 $arrayNumeri = array(); 
+
 for($i=0; $i< 15; $i++){
   $controllo = true;
-  while($controllo){ //ripeto fino a qauando non diventa false
+  while($controllo){ //ripeto fino a quando non diventa false
     $numero = rand(0,300);
     if(!in_array($numero, $arrayNumeri)){ //controllo che il numero non sia già presente
       $arrayNumeri[] = $numero; //aggiungo il numero all'array
